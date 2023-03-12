6ccc215771e701c22ecb784cb0217bf42bcbdd73
Mashinator2 <117071796+Mashinator2@users.noreply.github.com>
2023-03-12 18:26:13 +0600

    beka2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
      <p>hello world</p>
+     <button>Tap me</button>
      <div><img src="https://cdn.forbes.ru/files/1082x683/photo_galleries/1920-02_tcm-3173-1834873.jpg__1582289253__55999.webp" alt="" /></div>
     </div>
   )
